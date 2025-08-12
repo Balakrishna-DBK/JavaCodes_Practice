64f5922f2ced2f5e3caac59790e69c0dfbeba667
Bala Krishna <dontha.bala-krishna@capgemini.com>
2025-08-12 16:50:03 +0530

    Slight updated on pattern code

--- src/test/java/Patterns/P1.java
+++ src/test/java/Patterns/P1.java
@@ -5,7 +5,7 @@ public class P1 {
 	public static void main(String[] args) {
 
 		for(int i=1; i<=5; i++){
-			for(int j=i; j<=5; j++){
+			for(int j=1; j<=i; j++){
 				System.out.print(" * ");
 			}
 			System.out.println();
